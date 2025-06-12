2347eb2dc9ec0133b25a4c90a9b09746042120fb
safwan-kher <eng.safwan.kherallah@gmail.com>
2025-06-12 12:06:09 +0200

    adding mapTasks to  TaskProcessor class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,52 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Demo class
  *
  */
 public class App {
     public static void main( String[] args ) {
+        TaskProcessor processor = new TaskProcessor();
 
+        List<Task> tasks = Arrays.asList(
+                new Task("Buy groceries", false, 1),
+                new Task("Walk the dog", true, 2),
+                new Task("Read a book", false, 3)
+        );
+
+        // Filter tasks using lambda expression
+        List<Task> pendingTasks = processor.filterTasks(tasks, task -> !task.isCompleted());
+        //        task -> {
+        //            // do this
+        //            // then do that
+        //            return !task.isCompleted();
+        //        }
+
+
+        // Store tasks description using method reference
+        List<String> descriptions = processor.mapTasks(pendingTasks,Task::getDescription);
+        // task -> task.getDescription()
+
+
+        // Print all tasks using a stream
+       /* descriptions
+               // .stream() : this is optional. not bad, not good to use it here
+                .forEach(
+                      System.out::println
+                   // task -> System.out.println("Task -> " +task.getDescription())
+                );*/
+
+
+        // Converting numbers to strings
+        List<Integer> nums = List.of(1, 2, 3);
+        List<String> asStrings = nums.stream()
+                .map(number -> "Number: "+number)
+                .collect(Collectors.toList());
+
+        System.out.println(asStrings);
     }
 }
--- src/main/java/org/example/TaskProcessor.java
+++ src/main/java/org/example/TaskProcessor.java
@@ -1,5 +1,6 @@
 package org.example;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -8,11 +9,22 @@ import java.util.stream.Collectors;
  */
 public class TaskProcessor {
 
+    /**
+     * Filters tasks based on a given predicate.
+     */
     public List<Task> filterTasks(List<Task> tasks, Predicate<Task> condition){
 
         return tasks
                 .stream() // Convert the list to a Stream
                 .filter(condition)   // Apply the Predicate<Task> condition: condition.test(task), what returns true will remain.
                 .collect(Collectors.toList());  // Collect the result into a new List
+    }
+
+
+    public List<String> mapTasks(List<Task> tasks, Function<Task, String> mapper) {
+            return tasks
+                    .stream()
+                    .map(mapper)
+                    .collect(Collectors.toList());
     }
 }
